c7d5a245dc35a2b3928fd71914ca6ce6035e55a6
kmyles82 <kmyles82@gmail.com>
2019-11-02 22:29:30 -0500

    added text animations

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const intro = document.querySelector('.intro');
 const video = document.querySelector('video');
 const text = document.querySelector('h1');
+const revo = document.querySelector('#revo');
+const nice = document.querySelector('#nice');
+const xmas = document.querySelector('#xmas');
 //End section
 const section = document.querySelector('section');
 const end = section.querySelector('h1');
@@ -8,11 +11,58 @@ const end = section.querySelector('h1');
 //scroll magic
 const controller = new ScrollMagic.Controller();
 
-const scene = new ScrollMagic.Scene({
-        duration: 9000,
-        triggerElement: intro,
-        triggerHook: 0
-    })
-    .addIndicators()
-    .setPin(intro)
+//scenes
+let scene = new ScrollMagic.Scene({
+    duration: 9000,
+    triggerElement: intro,
+    triggerHook: 0
+})
+.addIndicators()
+.setPin(intro)
+.addTo(controller);
+
+//text animation
+const textAnim = TweenMax.fromTo(text, 3,{opacity: 1}, {opacity: 0})
+const revoAnim = TweenMax.fromTo(revo, 3,{opacity: 1}, {opacity: 0})
+const niceAnim = TweenMax.fromTo(nice, 3,{opacity: 1}, {opacity: 0})
+const xmasAnim = TweenMax.fromTo(xmas, 3, { opacity: 1 }, { opacity: 0 })
+
+let scene2 = new ScrollMagic.Scene({
+    duration: 3000,
+    triggerElement: intro,
+    triggerHook: 0
+})
+    .setTween(revoAnim)
     .addTo(controller);
+
+let scene3 = new ScrollMagic.Scene({
+    duration: 3000,
+    triggerElement: intro,
+    triggerHook: 0
+})
+    .setTween(niceAnim)
+    .addTo(controller);
+
+let scene4 = new ScrollMagic.Scene({
+    duration: 3000,
+    triggerElement: intro,
+    triggerHook: 0
+})
+    .setTween(xmasAnim)
+    .addTo(controller);
+
+
+
+//video animation
+let accelamount = 0.5;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on('update', e => {
+    scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+    delay += (scrollpos - delay) * accelamount;
+    video.currentTime = scrollpos;
+}, 180);
